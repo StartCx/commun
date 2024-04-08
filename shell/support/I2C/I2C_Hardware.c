20074b9beb09c25001bcc1957cf3696e7410320f
ECHO_START_END_TIME <3022235528@qq.com>
2024-04-08 19:44:34 +0800

    add i2chardware driver

--- shell/support/I2C/I2C_Hardware.c
+++ shell/support/I2C/I2C_Hardware.c
@@ -26,6 +26,8 @@ void I2C_M_Hardware_Config(I2C_M_Hardware_t *I2C_Driver)
 	I2C_Init(I2C_Driver->I2Cx, &I2C_InitStructure);
 	I2C_Cmd(I2C_Driver->I2Cx, ENABLE);
 	I2C_Driver->I2Cx->CR1 = 0x401;
+	I2C_Driver->I2Cx->SR1 = 0x00;
+	
 }
 
 
@@ -56,7 +58,7 @@ uint8_t I2C_M_Hardware_Result(I2C_M_Hardware_t *I2C_Driver)
 
 void I2C_M_Hardware_Set(I2C_M_Hardware_t *I2C_Driver,uint8_t Dev_Addr, uint16_t Reg_Addr, uint8_t Reg_Size, uint8_t *pData, uint16_t Size)
 {
-	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
+	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BERR) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_STOPF)){
 		I2C_M_Hardware_Config(I2C_Driver);
 	}
 	I2C_Driver->Delay_cnt = I2C_Driver->Delay_time;
@@ -125,9 +127,10 @@ ERROR:
 
 void I2C_M_Hardware_Get(I2C_M_Hardware_t *I2C_Driver, uint8_t Dev_Addr, uint16_t Reg_Addr,uint8_t Reg_Size, uint8_t *pData, uint16_t Size) 
 {
-	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
+	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BERR) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_STOPF)){
 		I2C_M_Hardware_Config(I2C_Driver);
 	}
+	
 	I2C_Driver->Delay_cnt = I2C_Driver->Delay_time;
 	while(I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
 		if((I2C_Driver->Delay_cnt--) == 0){
@@ -215,7 +218,7 @@ ERROR:
 
 void I2C_M_Hardware_Write(I2C_M_Hardware_t *I2C_Driver,uint8_t Dev_Addr, uint8_t *pData, uint16_t Size)
 {
-	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
+	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BERR) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_STOPF)){
 		I2C_M_Hardware_Config(I2C_Driver);
 	}
 	I2C_Driver->Delay_cnt = I2C_Driver->Delay_time;
@@ -262,7 +265,7 @@ ERROR:
 
 void I2C_M_Hardware_Read(I2C_M_Hardware_t *I2C_Driver, uint8_t Dev_Addr, uint8_t *pData, uint16_t Size) 
 {	
-	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
+	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BERR) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_STOPF)){
 		I2C_M_Hardware_Config(I2C_Driver);
 	}
 	I2C_Driver->Delay_cnt = I2C_Driver->Delay_time;
@@ -316,7 +319,7 @@ ERROR:
 
 void I2C_M_Hardware_Detect(I2C_M_Hardware_t *I2C_Driver, uint8_t _Address)
 {
-	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY)){
+	if( I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BUSY) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_BERR) || I2C_GetFlagStatus(I2C_Driver->I2Cx, I2C_FLAG_STOPF)){
 		I2C_M_Hardware_Config(I2C_Driver);
 	}
 	I2C_Driver->Delay_cnt = I2C_Driver->Delay_time;
